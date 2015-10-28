00dd535cf5e7f3fe79ffe1ee5cb69abe7ec46003
unknown <valeriya.syomina@yandex.ru>
2015-10-28 08:49:19 +0300

    AES class added

---
+++ src/EncryptionAlgorithms/AES128.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAlgorithms
+{
+    public class AES128
+    {
+        private const int Nr = 10;
+        private const int Nk = 4;
+        private const int Nb = 4;   // Блок информации = 4 слова, одно слово = 32 бита (32 * 4 = 128 бит)
+        private const int keySize = 16;  // размер секретного ключа = 16 байт (16 * 8 = 128 бит)
+        private byte[] secretKey;
+        private byte[] raundKeys;
+
+        public AES128() 
+        {
+            secretKey = new byte[keySize];
+            raundKeys = new byte[(Nr + 1) * Nb];
+        }
+        public void GenerateRaundKeys()
+        {
+            GenerateSecretKey();
+            for (int i = 0; i <= Nr; i++)
+            {
+
+            }
+        }
+        private void GenerateSecretKey()
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            random.NextBytes(secretKey);
+        }
+    }
+}
